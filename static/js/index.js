d5b40435130c84649cf37f53a489a8296d1df543
tim <tim@tslp.me>
2013-08-21 18:53:31 +0400

    try move to rickshaw from nvd3

--- static/js/index.js
+++ static/js/index.js
@@ -3,37 +3,48 @@
 
   var mychart;
 
-  nv.addGraph(function() {  
-    var chart = nv.models.lineChart()
-                  .showLegend(true)
-                  .margin({top: 10, bottom: 30, left: 40, right: 10});
+  // nv.addGraph(function() {  
+  //   var chart = nv.models.lineChart()
+  //                 .showLegend(true)
+  //                 .margin({top: 10, bottom: 30, left: 40, right: 10});
 
-    chart.xAxis
-       .tickFormat(function(d) {
-           console.warn(d);
-           return d3.time.format('%X')(new Date(d*1000))
-       });
+  //   chart.xAxis
+  //      .tickFormat(function(d) {
+  //          console.warn(d);
+  //          return d3.time.format('%X')(new Date(d*1000))
+  //      });
 
-    chart.yAxis
-        .tickFormat(d3.format(',.1f'));
+  //   chart.yAxis
+  //       .tickFormat(d3.format(',r'));
 
-  mychart = chart;
+  // mychart = chart;
 
-  d3.json("/data", function(error, json) {
-    //if (error) return console.warn(error);
-    //alert(error);
-    console.info(error);
+  // d3.json("/data", function(error, json) {
+  //   //if (error) return console.warn(error);
+  //   //alert(error);
+  //   console.info(error);
 
-    // d3.select('#exampleOne')
-    //     .datum(error)
-    //     .transition().duration(500)
-    //     .call(chart);
-  });
+  //   // d3.select('#exampleOne')
+  //   //     .datum(error)
+  //   //     .transition().duration(500)
+  //   //     .call(chart);
+  // });
 
 
-    nv.utils.windowResize(chart.update);
-    return chart;
-  });
+  //   nv.utils.windowResize(chart.update);
+  //   return chart;
+  // });
+
+var graph = new Rickshaw.Graph({
+        element: document.querySelector("#exampleOne"),
+        renderer: 'line',
+        series: [{
+                data: [ { x: 0, y: 40 }, { x: 1, y: 49 }],
+                color: 'steelblue'
+        }]
+});
+ 
+graph.render();
 
 
   updateSensors = function(values) {
@@ -44,16 +55,16 @@
       d3.select("#pressure").html(values.pressure + " mm");
       var datetime = new Date(values.time);
       d3.select("#time").html(datetime.getUTCHours() + ":" + datetime.getUTCMinutes());
-    d3.json("/data", function(error, json) {
-      //if (error) return console.warn(error);
-      //alert(error);
-      //console.info(error);
+    // d3.json("/data", function(error, json) {
+    //   //if (error) return console.warn(error);
+    //   //alert(error);
+    //   //console.info(error);
 
-      d3.select('#exampleOne')
-          .datum(error)
-          .transition().duration(500)
-          .call(mychart);
-    });
+    //   d3.select('#exampleOne')
+    //       .datum(error)
+    //       .transition().duration(500)
+    //       .call(mychart);
+    // });
   }
 
   var socket = io.connect('/shouts');
