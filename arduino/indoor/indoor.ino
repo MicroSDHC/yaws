0fe3a866145c8a5a48378508490f74fb743efe9f
Timothy Sleptsov <tim@tslp.me>
2013-10-19 23:47:30 +0400

    add retry on dht read and rfm12b send

--- arduino/indoor/indoor.ino
+++ arduino/indoor/indoor.ino
@@ -1,6 +1,5 @@
 #include <JeeLib.h>
 #include <DHT22.h>
-#include <stdio.h>
 
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
@@ -9,7 +8,19 @@ ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 #define INDOOR_NODE_ID  21
 #define BASE_STATION_ID 14
 
+#define DHT_ERROR_CNT_MAX 10
+#define ACK_TIME          10
+
+#define SEND_RETRY_LIMIT 10
+
 DHT22 myDHT22(DHT22_PIN);
+
+struct {
+  int temp;
+  int humi;
+  long vcc;
+  bool dht_error;
+} payload;
 
 void disableDHT22() {
   pinMode(DHT22_POWER_PIN, OUTPUT);
@@ -29,7 +40,8 @@ void blinkLed() {
   digitalWrite(9, LOW);
 }
 
-long readVcc() {
+
+void readVcc() {
   long result;
   // Read 1.1V reference against AVcc
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
@@ -39,7 +51,18 @@ long readVcc() {
   result = ADCL;
   result |= ADCH<<8;
   result = 1126400L / result; // Back-calculate AVcc in mV
-  return result;
+  payload.vcc = result;
+}
+
+
+static byte waitForAck() {
+    MilliTimer ackTimer;
+    while (!ackTimer.poll(ACK_TIME)) {
+        if (rf12_recvDone() && rf12_crc == 0 &&
+                rf12_hdr == (RF12_HDR_DST | RF12_HDR_CTL | INDOOR_NODE_ID))
+            return 1;
+    }
+    return 0;
 }
 
 void setup () {
@@ -48,45 +71,44 @@ void setup () {
   blinkLed();
 }
 
-int readDHT(char* buf)
+void readDHT()
 { 
+  byte retry_count = 0;
   DHT22_ERROR_t errorCode;
   enableDHT22();  
-  errorCode = myDHT22.readData();
-  switch(errorCode)
+  errorCode = DHT_ERROR_CHECKSUM;
+  while (errorCode != DHT_ERROR_NONE | retry_count < DHT_ERROR_CNT_MAX) {
+      errorCode = myDHT22.readData();
+      delay(2000);
+      retry_count++;
+  }
+  if (errorCode == DHT_ERROR_NONE)
   {
-    case DHT_ERROR_NONE:
-      sprintf(buf, "TEMP %hi.%01hi C, HUM %i.%01i %% RH, VCC %i mv\n",
-                   myDHT22.getTemperatureCInt()/10, abs(myDHT22.getTemperatureCInt()%10),
-                   myDHT22.getHumidityInt()/10, myDHT22.getHumidityInt()%10, readVcc());
-      Serial.println(buf);
-      break;
-    case DHT_ERROR_CHECKSUM:
-      break;
-    case DHT_BUS_HUNG:
-      break;
-    case DHT_ERROR_NOT_PRESENT:
-      break;
-    case DHT_ERROR_ACK_TOO_LONG:
-      break;
-    case DHT_ERROR_SYNC_TIMEOUT:
-      break;
-    case DHT_ERROR_DATA_TIMEOUT:
-      break;
-    case DHT_ERROR_TOOQUICK:
-      break;
+    payload.temp = myDHT22.getTemperatureCInt();
+    payload.humi = myDHT22.getHumidityInt();
+    payload.dht_error = false;
+  } else {
+    payload.dht_error = true;
   }
   disableDHT22();
-  return errorCode;
 }
 
 void loop () {
-  rf12_sleep(RF12_WAKEUP);
-  char buf[128];
-  readDHT(buf);
-  rf12_sendNow(BASE_STATION_ID, buf, strlen(buf));
-  rf12_sendWait(1);
-  rf12_sleep(RF12_SLEEP);
-  // go to sleep for approx 60 seconds
-  Sleepy::loseSomeTime(60000);
+  readDHT();
+  readVcc();
+  for (byte i = 0; i < SEND_RETRY_LIMIT; i++)
+  {
+    rf12_sleep(RF12_WAKEUP);
+    rf12_sendNow(BASE_STATION_ID, &payload, sizeof(payload));
+    rf12_sendWait(2);
+    byte acked = waitForAck();  
+    rf12_sleep(RF12_SLEEP);
+    if (acked)
+    {
+      return;
+    }
+    delay(1000);
+  }
+  // go to sleep for approx 60 * 5 seconds
+  Sleepy::loseSomeTime(60000 * 5);
 }
